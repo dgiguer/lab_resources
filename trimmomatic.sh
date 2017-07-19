62916e71d94dc0fb8b8fd29beb79497b3b109211
dgiguer <dgiguer@uwo.ca>
2017-07-19 16:31:47 -0400

    script works now

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 #July 19 2017 - Daniel Giguere
-#Run Trimmomatic to remove poor N-score reads and also the last couple of bases for each file in
-# a given working directory
+#Run Trimmomatic to remove poor N-score reads and also the last couple of bases for each file
 
-#setup path to data
-DATA="/Volumes/data/suncor/data/"
-
-#setup path to trimmomatic.jar file
-TRIM="/home/dgiguer/Trimmomatic-0.36/trimmomatic-0.36.jar"
-
-#make an output directory if it doesn't exist
-mkdir -p trimmomatic_output
+#set up a directory called trim_output before running.
 
 #run trimmomatic
-for f in $DATA/*fastq.gz; do
-  #get name of file
-  B='basename $f'
-  NAME='echo $B'
-
-  #run trimmomatic on each f
-  nohup java -jar $TRIM SE [-threads 32] $f trimmomatic_output/$NAME.trim.out LEADING:5 CROP:73
+for f in /Volumes/data/suncor/data/*fastq.gz
+do
+#get filename
+B=`basename $f`
+NAME=`echo $B`
+#run trimmomatic with 16 threads
+java -jar /Volumes/bin/Trimmomatic-0.36/trimmomatic-0.36.jar SE -threads 16 $f trim_output/$NAME.out LEADING:5 CROP:73
 done
