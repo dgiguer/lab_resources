51446c79a10cab71a4d2c0a7f895ab8c2989b164
Daniel Giguere <dgiguer@uwo.ca>
2017-09-12 11:14:26 -0400

    duplicate check runs without bash for loop

--- README.md
+++
@@ -1,1 +0,0 @@
-scripts that may be useful
--- duplicatecheck.py
+++ duplicatecheck.py
@@ -5,32 +5,41 @@
 # Currently set up to check for duplicates in first column only.
 # Make sure target file is encoded F-8 and Unix LF.
 
+# to use:
+# put in directory of choice, and put output to txt file:
+# python duplicatecheck.py > duplicate_output.txt
+
 import collections
 import hashlib
 import re
+import glob
 
 #make dictionary list
 d = collections.defaultdict(list)
 
-# choose file to search for duplicates (testx.txt)
 # 'r' = read only
-with open('testx.txt', 'r') as datafile:
-    for line in datafile:
 
-        #split by tab
-        line = re.split('\t', line)
+# generate list of files in cwd.
+files = glob.glob('*.m8')
 
-        #create hash for first column. choose column number (python
-        #starts at 0)
-        id = hashlib.sha256(line[0]).digest()
+for file in files:
+    with open(file, 'r') as datafile:
+        for line in datafile:
 
-        #use first 2 bytes as key, store rest in value.
-        k = id[0:2]
-        v = id[2:]
+            #split by tab
+            line = re.split('\t', line)
 
-        #check if any hashes match
-        if v in d[k]:
-            print "duplicate(s) found:", line[0]
-        else:
-            #merge byte back together to be stored in list d.
-            d[k].append(v)
+            #create hash for first column. choose column number (python
+            #starts at 0)
+            id = hashlib.sha256(line[0]).digest()
+
+            #use first 2 bytes as key, store rest in value.
+            k = id[0:2]
+            v = id[2:]
+
+            #check if any hashes match
+            if v in d[k]:
+                print "duplicates found in", file, ":", line[0]
+            else:
+                #merge byte back together to be stored in list d.
+                d[k].append(v)
