065585ce3ffba7c666c115afce154e91fdc87872
Daniel Giguere <dgiguer@uwo.ca>
2018-01-23 20:46:34 -0500

    (somewhat) better comments

--- contig_size_sort.py
+++ contig_size_sort.py
@@ -6,6 +6,7 @@
 #take contigs from final.contigs.fa from megahit output and sorts by size (less or greater than 1000 bp)
 
 #each contig has description of one line, and is followed by one line of sequence. Sort based on if length of the sequence is greater than 1000.
+#descriptor line contains the string "len=x" to describe length of sequence line that immediately follows, this is what's used to get length of contigs.
 
 import re
 
@@ -14,9 +15,9 @@ with open('subset_megahitoutput.txt', 'r') as file:
     s = file.readlines()
     b = []
     y = []
-    #enumerate will count line numbers
+    #enumerate will count line numbers and provide index for extending lists
     for ind, line in enumerate(s):
-        #only search the right lines (the ones that start with ">")
+        #only search the right lines (descriptor lines start with ">")
         if ">" in line[0]:
             #get length by regex
             m = re.search("len=[0-9]+", line)
@@ -26,7 +27,7 @@ with open('subset_megahitoutput.txt', 'r') as file:
             length = int(match.split("=")[1])
             #decision tree if number is greater or less than x
             x=1000
-            #if greater than x, print line and line+1 to another file or do opposite for less than
+            #if integer is less than x, print line and line+1 (descriptor, and sequence) to separate lists
             if length < x:
                 b.extend([s[ind], s[ind+1]])
             else:
