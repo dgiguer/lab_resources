8c66ae369a46482db8ea33ca44a139e39c72634b
Daniel Giguere <dgiguer@uwo.ca>
2018-09-12 09:46:04 -0400

    added useful grep command for searching a fastq file

--- bash/one_liners.sh
+++ bash/one_liners.sh
@@ -3,6 +3,7 @@
 
 ################################################################################
 
+#sed
 #switch the ID and identifier field output.
 
 head all_ORFs.fasta
@@ -15,3 +16,12 @@ head all_ORFs_idswitched.fasta
 #>ID=1_1 # 3 # 236 # -1 # k79_1_1;partial=11;start_type=Edge;
 
 ################################################################################
+
+#grep
+
+#from a list of read ids, search a fastq file and retrieve the fastq info
+# no-group-separator prevents "--" placed on new lines after each match
+# A -3, get 3 lines after match
+# -h suppress file name output
+# -f from file (one match per line)
+grep --no-group-separator -A 3 -h -f interesting_read_ids.txt all_reads.fastq > output.fastq
