d97632cd5e722e7595678f8d68120feac2ebd515
Daniel Giguere <dgiguer@uwo.ca>
2018-09-14 14:45:55 -0400

    added conversion to fasta

--- bash/one_liners.sh
+++ bash/one_liners.sh
@@ -23,3 +23,6 @@ head all_ORFs_idswitched.fasta
 # search each read name in second file
 # print the line where match occurs, as well as following three lines
 awk 'NR==FNR{hash[$1];next} $1 in hash{print; nr[NR+1]; nr[NR+2]; nr[NR+3]; next} NR in nr' read_ids.txt all_reads.fastq > subset_reads.fastq
+
+#extract reads from fastq file, convert to fasta
+awk 'NR==FNR{hash[$1];next} $1 in hash{print ">" substr($0, 2); nr[NR+1]; next} NR in nr' read_names.txt all_reads.fastq > subset.fasta
